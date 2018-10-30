019acc0671c3d89c65a4039fcb1403bf5d9c1201
Chan Lee <chanethanlee@gmail.com>
2018-10-29 22:40:26 -0700

    apply get and post method.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,11 +8,24 @@ import { Http } from '@angular/http';
 })
 export class PostsComponent {
   posts: any[];
-  constructor(http: Http) { 
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  private url ='https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: Http) { 
+    http.get(this.url)
     //three overloads
       .subscribe(response => {
         this.posts = response.json();
+      });
+  }
+
+  createPost(input: HTMLInputElement){
+    let post = {title: input.value};
+    input.value= '';
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post["id"] = response.json().id;
+        this.posts.push(post);
+        //or splice(0,0,post) when unshift
+        console.log(response.json());
       });
   }
 }
